d9cd6566ea30d938d877898250f4e7e837c39eea
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-01-22 00:28:14 -0800

    Fix: Fix tests and missing import

--- main.go
+++ main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"github.com/bsphere/le_go"
-	"github.com/kelseyhightower/envconfig"
-	"github.com/ml-tv/tv-api/src/core/logger"
-	"github.com/ml-tv/tv-api/src/core/notifiers/mailer"
-	"github.com/ml-tv/tv-api/src/core/storage/db"
-)
-
-// Args represents the app args
-type Args struct {
-	Port            string `default:"5000"`
-	PostgresURI     string `required:"true" envconfig:"postgres_uri"`
-	LogEntriesToken string `envconfig:"logentries_token"`
-	EmailAPIKey     string `envconfig:"email_api_key"`
-	EmailFrom       string `envconfig:"email_default_from"`
-	EmailTo         string `envconfig:"email_default_to"`
-	Debug           bool   `default:"false"`
-}
+import "github.com/ml-tv/tv-api/src/components/api"
 
 func main() {
-	var params Args
-	if err := envconfig.Process("", &params); err != nil {
-		panic(err)
-	}
-
-	if err := db.Setup(params.PostgresURI); err != nil {
-		panic(err)
-	}
-
-	// LogEntries
-	if params.LogEntriesToken != "" {
-		le, err := le_go.Connect(params.LogEntriesToken)
-		if err != nil {
-			panic(err)
-		}
-		logger.LogEntries = le
-	}
-
-	// Sendgrid
-	if params.EmailAPIKey != "" {
-		mailer.Emailer = mailer.NewMailer(params.EmailAPIKey, params.EmailFrom, params.EmailTo)
-	}
+	api.Setup()
 }
--- src/core/router/router_test.go
+++ src/core/router/router_test.go
@@ -1,5 +1,7 @@
 package router_test
 
+import "github.com/ml-tv/tv-api/src/components/api"
+
 func init() {
-  
+	api.SetupIfNeeded()
 }
