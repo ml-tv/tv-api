8d50052520b5a863bf89778c35db1f73baa9cc23
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-02-12 22:33:55 -0800

    fix(user): Fix default sql value for is_admin

--- db/migrations/20170211211305_AddUserIsAdmin.sql
+++ db/migrations/20170211211305_AddUserIsAdmin.sql
@@ -2,7 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-ALTER TABLE users ADD COLUMN is_admin boolean;
+ALTER TABLE users ADD COLUMN is_admin boolean NOT NULL DEFAULT FALSE;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
